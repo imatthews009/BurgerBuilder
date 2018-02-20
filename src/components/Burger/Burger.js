3712fd51b3357a3fe1c4db439d7941260cac4f66
Ian <imatthews009@gmail.com>
2018-02-20 14:14:57 -0800

    added ingredients from state passed on burger componenet"

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,20 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
+  let arrayTest = [];
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // arrayTest.push([...Array(props.ingredients[igKey])].map((_, i) => igKey + i));
+      // console.log([...Array(props.ingredients[igKey])].map((_, i) => igKey + i));
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />
+      });
+    });
+  // console.log(arrayTest);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top'/>
-      <BurgerIngredient type='cheese'/>
-      <BurgerIngredient type='meat'/>
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom'/>
     </div>
   );
