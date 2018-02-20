f2945c7a5a0dc3d8693d218934fac44429966e4b
Ian <imatthews009@gmail.com>
2018-02-20 14:36:24 -0800

    flattened transformedIngredients and adjusted state default ingredients to 0

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
   let arrayTest = [];
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
-      // arrayTest.push([...Array(props.ingredients[igKey])].map((_, i) => igKey + i));
-      // console.log([...Array(props.ingredients[igKey])].map((_, i) => igKey + i));
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
       });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
     });
-  // console.log(arrayTest);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }  
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top'/>
